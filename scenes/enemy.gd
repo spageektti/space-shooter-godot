c9ca8597a215b959044ce20ebc0249ccbf5d8702
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-26 14:48:07 +0200

    enemy

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,17 +1,31 @@
 extends RigidBody2D
 
+@onready var player = %CharacterBody2D
+
 @export var bullet : PackedScene
+@export var health : int = 4
+
 @export var moving_x : bool
 @export var moving_y : bool
-
 @export var speed_x : float = 100.0
 @export var speed_y : float = 100.0
 
-var direction = 0 # 0 left 1 right
+var direction = -1 # -1 left 1 right
 
 func _process(delta):
-	var velocity = Vector2(0, 0)
-	velocity.x = speed_x * delta if moving_x else 0
-	velocity.y = speed_y * delta if moving_y else 0
+	position.x += direction * speed_x * delta if moving_x else 0
+	position.y += speed_y * delta if moving_y else 0
 	
-	set_axis_velocity(velocity)
+
+func damage():
+	health -= 1
+	print("Enemy damaged")
+	if(health == 0):
+		print("Enemy destroyed")
+		queue_free()
+
+func _on_world_border_right_body_entered(body):
+	direction = -1
+
+func _on_world_border_left_body_entered(body):
+	direction = 1
